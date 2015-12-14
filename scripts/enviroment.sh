9a7e52db7236b158051cc3f003732367f3d8341e
Anders Grahn <Anders Grahn>
2015-12-14 19:21:28 +0100

    Changed how the system language is selected

--- scripts/enviroment.sh
+++ scripts/enviroment.sh
@@ -4,6 +4,5 @@ echo -e "# KEYBOARD CONFIGURATION FILE\n# Consult the keyboard(5) manual page.\n
 echo "set timezone stockholm"
 timedatectl set-timezone Europe/Stockholm
 echo "change system language to swedish"
-locale-gen sv_SE.UTF-8
-update-locale LANG=sv_SE.UTF-8
+dpkg-reconfigure locales
 echo "reboot to apply changes"
