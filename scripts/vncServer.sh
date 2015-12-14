63e6c20a5a9f2caca09d013575cee6208c92c783
Anders Grahn <Anders Grahn>
2015-12-14 17:39:22 +0100

    Also starting tightvncserver, so that the password will be set

--- scripts/vncServer.sh
+++ scripts/vncServer.sh
@@ -1,7 +1,9 @@
 #!/bin/bash          
-	echo Installing tightVNC server
+	echo Installing tightVNC server on screen number :1
 	sudo apt-get -y install tightvncserver
 	
 	#See to it that the server starts at boot
 	mkdir /home/pi/.config/autostart
 	echo -e "[Desktop Entry]\nType=Application\nName=TightVNC\nExec=vncserver :1\nStartupNotify=false" > /home/pi/.config/autostart/tightvnc.desktop
+	#Set the password
+	tightvncserver
