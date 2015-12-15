03d94e880e4720f809136715edd3c83c3636779d
Anders Grahn <Anders Grahn>
2015-12-15 12:54:18 +0100

    Added script for setting a USB sound card as default sound

---
+++ scripts/usbSoundCardSetDefault.sh
@@ -0,0 +1,3 @@
+#!/bin/bash          
+	#Add config file to set the USB sound card as default, check if it is at 1 by entering: cat /proc/asound/cards
+	echo -e "pcm.!default {\ntype hw\ncard 1\n}\nctl.!default {\ntype hw\ncard 1\n}\n" > /etc/asound.conf
