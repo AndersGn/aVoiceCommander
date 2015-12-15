459d91c58a990b71f353fb2f659cd3890aab27f4
Anders Grahn <Anders Grahn>
2015-12-15 13:10:27 +0100

    added info on how to unmute the mic

--- scripts/usbSoundCardSetDefault.sh
+++ scripts/usbSoundCardSetDefault.sh
@@ -1,3 +1,4 @@
 #!/bin/bash          
 	#Add config file to set the USB sound card as default, check if it is at 1 by entering: cat /proc/asound/cards
 	echo -e "pcm.!default {\ntype hw\ncard 1\n}\nctl.!default {\ntype hw\ncard 1\n}\n" > /etc/asound.conf
+	# if needed unmute the mic with: amixer sset 'Mic',0 unmute
