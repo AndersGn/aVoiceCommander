3bb1ad7bffa1f2954d1ce7cdbe464681bb8c1270
Anders Grahn <Anders Grahn>
2015-12-15 14:10:59 +0100

    Added so that some commands are run as pi user

--- scripts/vncServer.sh
+++ scripts/vncServer.sh
@@ -3,7 +3,7 @@
 	sudo apt-get -y install tightvncserver
 	
 	#See to it that the server starts at boot
-	mkdir /home/pi/.config/autostart
-	echo -e "[Desktop Entry]\nType=Application\nName=TightVNC\nExec=vncserver :1\nStartupNotify=false" > /home/pi/.config/autostart/tightvnc.desktop
-	#Set the password
-	tightvncserver
+	sudo -i -u pi mkdir /home/pi/.config/autostart
+	sudo -i -u pi echo -e "[Desktop Entry]\nType=Application\nName=TightVNC\nExec=vncserver :1\nStartupNotify=false" > /home/pi/.config/autostart/tightvnc.desktop
+	#Set the password (as the pi user)
+	sudo -i -u pi tightvncserver
