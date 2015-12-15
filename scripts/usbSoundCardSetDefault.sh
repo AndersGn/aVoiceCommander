1dc3d0153a2e12a27ae8780573bee764afad48c4
AndersGn <johndoe@example.com>
2015-12-15 13:22:21 +0100

    Added default sound for pi user

--- scripts/usbSoundCardSetDefault.sh
+++ scripts/usbSoundCardSetDefault.sh
@@ -1,3 +1,6 @@
 #!/bin/bash          
-	#Add config file to set the USB sound card as default, check if it is at 1 by entering: cat /proc/asound/cards
+	#Add config file to set the USB sound card as default system wide, check if it is at 1 by entering: cat /proc/asound/cards
 	echo -e "pcm.!default {\ntype hw\ncard 1\n}\nctl.!default {\ntype hw\ncard 1\n}\n" > /etc/asound.conf
+	#Add config file to set default card for pi user
+	echo -e "pcm.!default sysdefault:Device" > /home/pi/.asoundrc
+
