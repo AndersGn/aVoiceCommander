3e3a0cbc979c906b9bc53ea9ad2f822b952fc53d
AndersGn <my@example.com>
2016-01-01 14:46:10 +0100

    Added execution bit on script file

--- scripts/PiCamera.sh
+++ scripts/PiCamera.sh
@@ -2,6 +2,6 @@
 	echo activate the raspberry pi camera
 	sudo raspi-config
 	echo Installing PiCamera dependencies
-	sudo apt-get -y install python-picamera python3-picamera python-rpi.gpio build-essential libavformat-dev ffmpeg libcv2.4 libcvaux2.4 libhighgui2.4 python-opencv opencv-doc libcv-dev libcvaux-dev libhighgui-dev python-numpy
+	sudo apt-get -y install python-picamera python3-picamera python-rpi.gpio build-essential libavformat-dev libcv2.4 libcvaux2.4 libhighgui2.4 python-opencv opencv-doc libcv-dev libcvaux-dev libhighgui-dev python-numpy
 	echo Copying the examples to home/pi/
 	cp -r /usr/share/doc/opencv-doc/examples /home/pi/.
