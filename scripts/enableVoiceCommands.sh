84f46a21a0e1638b4da34fce6bbe1754734d35c4
Anders Grahn <Anders Grahn>
2016-01-01 21:40:06 +0100

    Adding pocketsphinx to the setup

--- scripts/enableVoiceCommands.sh
+++ scripts/enableVoiceCommands.sh
@@ -1,6 +1,7 @@
 #!/bin/bash          
-	#Add espeak (to get make it speak :-)
+	echo Add espeak (to get make it speak :-)
 	apt-get install -y espeak
 	# Test the voice
 	espeak "hello"
-
+	echo Install voice command software and its dependencies (pocketsphinx)
+	apt-get install -y sphinxbase pocketsphinx sphinxtrain
