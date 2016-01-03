67cf75b3a957ff5c7065bf5fa39e4c0514e2a911
Anders Grahn <Anders Grahn>
2016-01-03 14:19:34 +0100

    adding emacs to enviroment

--- scripts/enviroment.sh
+++ scripts/enviroment.sh
@@ -1,4 +1,8 @@
-#!/bin/bash 
+#!/bin/bash
+echo "update and install packages"
+sudo apt-get update
+sudo apt-get install -y emacs
+ 
 #Change the keyboard
 echo "set swedish keyboard"
 echo -e "# KEYBOARD CONFIGURATION FILE\n# Consult the keyboard(5) manual page.\nXKBMODEL=\"pc105\"\nXKBLAYOUT=\"se\"\nXKBVARIANT=""\nXKBOPTIONS=\"lv3:ralt_switch\"\nBACKSPACE=\"guess\"" > /etc/default/keyboard
@@ -14,3 +18,4 @@ read input_variable
 echo -e $input_variable > /etc/hostname
 sudo /etc/init.d/hostname.sh
 echo "reboot to apply changes"
+	
